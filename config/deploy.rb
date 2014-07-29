5cd4afc0d7c77cb96a2ad06e71bcce1608d68a31
Yuri <yuribuerov@gmail.com>
2014-07-29 14:50:50 +0300

    capistrano configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,14 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'lits'
+set :repo_url, 'git@github.com:YuriBuerov/lits.git'
+
+set :deploy_to, '/home/deploy/lits'
+
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
@@ -40,7 +46,7 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+       execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
