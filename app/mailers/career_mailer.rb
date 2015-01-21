fe7d4fa6317378645a3c07f60310e903ef99c059
cybdoom <cybdoom@gmail.com>
2015-01-21 13:28:45 +0200

    Removed precompiled assets Implemented reply mails notification

--- app/mailers/career_mailer.rb
+++ app/mailers/career_mailer.rb
@@ -6,4 +6,9 @@ class CareerMailer < ActionMailer::Base
     User.admins.each { |admin| mail(to: admin.email, subject: I18n.t('career.mail.vacancy_created.subject')) }
   end
 
+  def reply_created(reply)
+    @reply = reply
+    mail(to: reply.vacancy.user.email, subject: I18n.t('career.mail.reply_created.subject'))
+  end
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,19 @@ Lits::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => "logic-it-solutions" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => "gmail.com",
+    :user_name            => "lits.mailer@gmail.com",
+    :password             => 'lits123!@#',
+    :authentication       => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
