d72019b9e3e76a4d371353b2a15af59827dab321
cybdoom <cybdoom@gmail.com>
2015-01-27 15:40:37 +0200

    Updated unicorn config

--- config/unicorn/staging.rb
+++ config/unicorn/staging.rb
@@ -1,7 +1,7 @@
 deploy_to    =  "/var/www/devlits.com/devlits.com/docroot"
 rails_root   =  "#{deploy_to}/current"
-pid_file     =  "#{deploy_to}/shared/pids/lits_unicorn.pid"
-socket_file  =  "#{deploy_to}/shared/lits_unicorn.sock"
+pid_file     =  "#{rails_root}/tmp/pids/unicorn.pid"
+socket_file  =  "#{rails_root}/tmp/sockets/unicorn.sock"
 log_file     =  "#{rails_root}/log/unicorn.log"
 err_log      =  "#{rails_root}/log/unicorn_error.log"
 old_pid      =  pid_file + '.oldbin'
